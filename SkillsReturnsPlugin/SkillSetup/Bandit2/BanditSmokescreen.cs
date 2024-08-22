cff933268cc502b0a195faac626775ef29fd1d99
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-22 13:27:19 -0700

    Marked Combat Knife icon as Sprite

---
+++ SkillsReturnsPlugin/SkillSetup/Bandit2/BanditSmokescreen.cs
@@ -0,0 +1,11 @@
+﻿using RoR2.Skills;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillsReturns.SkillSetup.Bandit2
+{
+    public class BanditSmokescreen : SkillBase<BanditSmokescreen>
+    {
+    }
+}
